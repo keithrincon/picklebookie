39fe2a32f973f654054a2c82e37e3b027dcbb037
Keith Rincon <keithrt3008@gmail.com>
2025-03-24 17:07:16 -0700

    update bugs

--- frontend/src/firebase/createUserDocument.js
+++ frontend/src/firebase/createUserDocument.js
@@ -1,5 +1,5 @@
 import { doc, setDoc, getDoc } from 'firebase/firestore';
-import { db } from '@firebase/firebase';
+import { db } from '../firebase/firebase';
 
 export const createUserDocument = async (user, additionalData = {}) => {
   if (!user || !user.uid) {
